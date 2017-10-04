a847347a461a8eb4ee5f960524c5e0d48460b401
amarkpark <amarkpark@gmail.com>
2017-10-04 23:46:13 +0000

    initial movies MVC

---
+++ db/migrate/20171004231628_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :release_year
+      t.text :review
+      t.integer :rating
+      t.timestamps null: false
+    end
+  end
+end
