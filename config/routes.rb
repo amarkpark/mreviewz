b04f121164e1266f232eed88811755119cefd292
amarkpark <amarkpark@gmail.com>
2017-10-05 19:51:51 +0000

    Final touches

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
   resources :movies
-
-
-  get 'static_pages/index'
+  get 'static_pages/credits' => 'static_pages#credits', as: :credits
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
