61e1f65425286212326b2e592ae5c7f9c807b23c
amarkpark <amarkpark@gmail.com>
2017-10-05 04:00:33 +0000

    added create action and strong params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,20 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    Movie.create(movie_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :release_year, :rating, :review)
+  end
+
 end
+
+# :name
+# :release_year 
+# :rating 
+# :review 
